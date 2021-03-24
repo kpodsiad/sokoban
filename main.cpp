750786d14f5b63e80ffcf1a2a6aa1b48ea2afd17
kpodsiadlo <kamilpodsiadlo44@gmail.com>
2021-03-24 21:56:32 +0100

    isSolved function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
 enum Field {
-	wall = '#', chest = '0', target = '*', worker = '-', newline = '\n', empty = ' '
+	wall = '#', box = '0', target = '*', worker = '-', newline = '\n', empty = ' '
 };
 
-typedef map<tuple<size_t, size_t>, char> Board;
+typedef tuple<size_t, size_t> Point;
+typedef map<Point, char> Board;
 
 Board &readBoard(size_t &width, size_t &height, Board &board);
+bool isSolved(Board &board, vector<Point> &locations);
 
 int main() {
 	size_t height = 0, width = 0;
@@ -44,4 +47,14 @@ Board &readBoard(size_t &width, size_t &height, Board &board) {
 	}
 	cin.setf(ios_base::skipws);
 	return board;
+}
+
+bool isSolved(Board &board, vector<Point> &locations) {
+	for (auto &it : locations) {
+		auto found = board.find(it);
+		if (found == board.end() || found->second != box) {
+			return false;
+		}
+	}
+	return true;
 }
